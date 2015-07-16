9fd40ed27d62bcbba660ad3c42f2029cdaad2247
Chris C Cerami <chrisccerami@gmail.com>
2015-07-16 00:42:45 -0400

    Brush stroke can be changed

--- painter.rb
+++ painter.rb
@@ -1,9 +1,18 @@
 class Painter
+  require 'canvas'
   def initialize
     @canvas = Canvas.instance
-    @brush = Brush.new(".", 0, 0)
+    @brush = Brush.new('.', 0, 0)
     at_exit { canvas.close }
   end
+
+  COMMAND_KEYS = [
+    Canvas::KEY_UP,
+    Canvas::KEY_DOWN,
+    Canvas::KEY_LEFT,
+    Canvas::KEY_RIGHT,
+    27
+  ]
 
   def paint
     canvas.paint(brush.x_pos, brush.y_pos, brush.stroke)
@@ -17,14 +26,23 @@ class Painter
   attr_reader :canvas, :brush
 
   def accept_input
+    char = canvas.accept_input
+    if COMMAND_KEYS.include?(char)
+      system_commands(char)
+    else
+      brush.stroke = char
+    end
+    canvas.paint(brush.x_pos, brush.y_pos, brush.stroke)
+  end
+
+  def system_commands(char)
     inputs = {
       Canvas::KEY_UP => proc { brush.move_up },
       Canvas::KEY_LEFT => proc { brush.move_left },
       Canvas::KEY_DOWN => proc { brush.move_down },
       Canvas::KEY_RIGHT => proc { brush.move_right },
-      q: proc { ui.close }
+      27 => proc { canvas.close }
     }
-    inputs[canvas.accept_input].call
-    canvas.paint(brush.x_pos, brush.y_pos, brush.stroke)
+    inputs[char].call
   end
 end
